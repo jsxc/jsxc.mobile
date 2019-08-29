e18712ebc7310be9172c700d68885e8d9a95f66d
Abdelrahman Abdelhafez <abdelrahman@maged.rocks>
2019-08-29 15:50:41 +0200

    :sparkles: Facilitate basic login

--- www/js/index.js
+++ www/js/index.js
@@ -3,14 +3,56 @@ const initialize = () => {
 };
 
 const onDeviceReady = () => {
-  const parentElement = document.getElementById('deviceready');
-  const listeningElement = parentElement.querySelector('.listening');
-  const receivedElement = parentElement.querySelector('.received');
+  console.log('✅ Device is ready');
 
-  listeningElement.setAttribute('style', 'display:none;');
-  receivedElement.setAttribute('style', 'display:block;');
+  const domain = 'uni-konstanz.de';
+  const url = 'https://xmpp.uni-konstanz.de:5280/http-bind/';
 
-  console.log('Device is ready');
+  const jsxc = initializeJSXC({
+    domain,
+    url,
+  });
+
+  $('#login').click(() => {
+    const username = $('#username').val();
+    const password = $('#password').val();
+
+    connectToXMPPServer({
+      jsxc,
+      domain,
+      url,
+      username,
+      password,
+    });
+
+    return false;
+  });
+};
+
+const initializeJSXC = ({ domain, url }) => {
+  return new JSXC({
+    loadConnectionOptions: () => {
+      return Promise.resolve({
+        xmpp: {
+          domain,
+          url,
+        },
+      });
+    },
+  });
+};
+
+const connectToXMPPServer = ({ jsxc, domain, url, username, password }) => {
+  const jid = [username, '@', domain].join('');
+
+  jsxc
+    .start(url, jid, password)
+    .then(() => {
+      console.log('✅ Connected to XMPP server');
+    })
+    .catch(() => {
+      console.log('❌ Failed to connect to XMPP server');
+    });
 };
 
 initialize();
