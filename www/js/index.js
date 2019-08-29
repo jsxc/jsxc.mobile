cef8bcb1379eca8e598007058953712f0f27007b
Abdelrahman Abdelhafez <abdelrahman@maged.rocks>
2019-08-29 14:53:58 +0200

    :art: Clean up code

--- www/js/index.js
+++ www/js/index.js
@@ -1,23 +1,5 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-  // Application Constructor
+const app = {
+  /* Application constructor */
   initialize: function() {
     document.addEventListener(
       'deviceready',
@@ -25,20 +7,21 @@ var app = {
       false,
     );
   },
-
-  // deviceready Event Handler
-  //
-  // Bind any cordova events here. Common events are:
-  // 'pause', 'resume', etc.
+  /**
+   *  deviceready event handler
+   *
+   *  Bind any cordova events here. Common events are:
+   *  'pause', 'resume', etc.
+   */
   onDeviceReady: function() {
     this.receivedEvent('deviceready');
   },
 
-  // Update DOM on a Received Event
+  /* Update DOM on a received event */
   receivedEvent: function(id) {
-    var parentElement = document.getElementById(id);
-    var listeningElement = parentElement.querySelector('.listening');
-    var receivedElement = parentElement.querySelector('.received');
+    const parentElement = document.getElementById(id);
+    const listeningElement = parentElement.querySelector('.listening');
+    const receivedElement = parentElement.querySelector('.received');
 
     listeningElement.setAttribute('style', 'display:none;');
     receivedElement.setAttribute('style', 'display:block;');
