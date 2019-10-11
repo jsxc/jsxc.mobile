dd28e07e89429cd08181e48d14d7707e12f1f6b1
Abdelrahman Abdelhafez <abdelrahman@maged.rocks>
2019-10-11 17:11:22 +0200

    :sparkles: Connect to XMPP BOSH server

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Strophe } from 'strophe.js';
+import { decodeConnectionStatusConstant } from './xmpp';
+
+const { Connection } = Strophe;
 
 export default function App() {
+  useEffect(() => {
+    const url = 'https://example.com:5280/http-bind/';
+    const username = 'john.doe@example.com';
+    const password = '123456';
+
+    const connection = new Connection(url);
+
+    connection.connect(username, password, status => {
+      console.log('onConnect >', decodeConnectionStatusConstant(status));
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
