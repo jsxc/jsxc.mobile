45258522e24bc8feb848eb175854f571bb264116
Abdelrahman Abdelhafez <abdelrahman@maged.rocks>
2019-10-22 10:35:10 +0200

    :lipstick: Build basic Login view

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,20 +1,66 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet } from 'react-native';
+import { Layout, Text, Input, Button } from 'react-native-ui-kitten';
 
 const Login = () => {
+  const [url, setUrl] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
-    <View style={styles.container}>
-      <Text>Login</Text>
-    </View>
+    <Layout style={styles.container}>
+      <Text style={styles.header} category="h3">
+        Login
+      </Text>
+
+      <Input
+        style={styles.textInput}
+        label="URL"
+        value={url}
+        onChangeText={setUrl}
+      />
+
+      <Input
+        style={styles.textInput}
+        label="Username"
+        value={username}
+        onChangeText={setUsername}
+      />
+
+      <Input
+        style={styles.textInput}
+        label="Password"
+        secureTextEntry={true}
+        value={password}
+        onChangeText={setPassword}
+      />
+
+      <Button
+        style={styles.button}
+        onPress={() => {
+          console.log({ url, username, password });
+        }}
+      >
+        Submit
+      </Button>
+    </Layout>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  header: {
+    margin: 8,
+  },
+  textInput: {
+    margin: 8,
+  },
+  button: {
+    margin: 8,
   },
 });
 
